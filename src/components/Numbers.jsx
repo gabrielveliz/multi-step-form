dd41f734f8a76403b1065b9139de4746d7042ca6
Gabriel Veliz <gvelizzuniga@gmail.com>
2024-06-04 17:02:34 -0400

    trabajando en Step1

--- src/components/Numbers.jsx
+++ src/components/Numbers.jsx
@@ -11,8 +11,8 @@ const Numbers = () =>{
                         <div className="NumberBox"><span className="Number">1</span></div>
                     </div>
                     <div>
-                        <div><span>STEP 1</span></div>
-                        <div><span>YOUR INFO</span></div>
+                        <div className="desctitl"><span>STEP 1</span></div>
+                        <div className="descnumb"><span>YOUR INFO</span></div>
                     </div>
                 </div>
 
@@ -21,8 +21,8 @@ const Numbers = () =>{
                         <div className="NumberBox"><span>2</span></div>
                     </div>
                     <div>
-                        <div><span>STEP 2</span></div>
-                        <div><span>SELECT PLAN</span></div>
+                        <div className="desctitl"><span>STEP 2</span></div>
+                        <div className="descnumb"><span>SELECT PLAN</span></div>
                     </div>
                 </div>
 
@@ -31,8 +31,8 @@ const Numbers = () =>{
                         <div className="NumberBox"><span>3</span></div>
                     </div>
                     <div>
-                        <div><span>STEP 3</span></div>
-                        <div><span>ADD-ONS</span></div>
+                        <div className="desctitl"><span>STEP 3</span></div>
+                        <div className="descnumb"><span>ADD-ONS</span></div>
                     </div>
                 </div>
 
@@ -41,8 +41,8 @@ const Numbers = () =>{
                         <div className="NumberBox"><span>4</span></div>
                     </div>
                     <div>
-                        <div><span>STEP 4</span></div>
-                        <div><span>SUMMARY</span></div>
+                        <div className="desctitl"><span>STEP 4</span></div>
+                        <div className="descnumb"><span>SUMMARY</span></div>
                     </div>
                 </div>
 
--- src/components/Step1.jsx
+++ src/components/Step1.jsx
@@ -1,8 +1,28 @@
+import "../styles/Step1.css";
 
 const Step1 = () =>{
     return(
         <>
-            <h1>Step1sadfsaddddddddadsssssss</h1>
+                <div className="seg tit">
+                    <h2>Personal info</h2>
+                    <span>Please provide your name, email address, and phone number.</span>
+                </div>
+                <div className="seg for">
+                    <span className="lab">Name</span>
+                    <input type="text" placeholder="e. g. Stephen King"/>
+                </div>
+                <div className="seg">
+                    <span className="lab">Email Address</span>
+                    <input type="text" placeholder="e. g. stephenking@lorem.com"/>
+                </div>
+                <div className="seg">
+                    <span className="lab">Phone Number</span>
+                    <input type="text" placeholder="e. g. +1 234 567 890" />
+                </div>
+                <div className="seg segend">
+                    <button>Next Step</button>
+                </div>
+
         </>
     )
 }
